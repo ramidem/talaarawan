70ced972ca36795d0fc8e39e199a7ed9f881f23f
Med Aduh <im.ramidem@gmail.com>
2023-03-08 18:08:18 +0800

    [feat] get tasks from js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,68 @@
-console.log("Hellow, World!");
+const tasks = [
+  {
+    id: 1,
+    content: "something to do",
+    type: "task",
+    status: null,
+  },
+  {
+    id: 2,
+    content: "tomorrow is another day",
+    type: "note",
+    status: null,
+  },
+  {
+    id: 3,
+    content: "started working on talaarawan",
+    type: "event",
+    status: null,
+  },
+  {
+    id: 4,
+    content: "something done",
+    type: "task",
+    status: "done",
+  },
+  {
+    id: 5,
+    content: "this is not important",
+    type: "task",
+    status: "cancelled",
+  },
+];
+
+const getKey = (task) => {
+  switch (task.type) {
+    case "task":
+      if (task.status === "done") {
+        return "x";
+      }
+      return "*";
+
+    case "note":
+      return "-";
+
+    case "event":
+      return "=";
+
+    default:
+      return "*";
+  }
+};
+
+const ul = document.getElementById("tasks");
+
+tasks.forEach((task) => {
+  const li = document.createElement("li");
+  const content = document.createTextNode(`${getKey(task)} ${task.content}`);
+
+  if (task.status === "cancelled") {
+    const em = document.createElement("em");
+    em.appendChild(content);
+    li.appendChild(em);
+  } else {
+    li.appendChild(content);
+  }
+
+  ul.appendChild(li);
+});
